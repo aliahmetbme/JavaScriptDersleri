9ac0a4ce03289a0ced2c4ae40db7d1734395a916
aliahmetbme <082aliahmet4@gmail.com>
2023-03-01 18:43:47 +0300

    sdjs

---
+++ giris.js
@@ -0,0 +1,9 @@
+// console logla merhaba world demek
+console.log("hello world");
+
+// mesaj çıkarmak
+// browserda gösterir bu şekilde
+//alert("merhaba dünya")
+
+// dokümana yazma 
+//document.write("hello kaan")
