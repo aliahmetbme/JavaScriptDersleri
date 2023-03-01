b637b04555903d0e83037eb5db495a626a1bba70
aliahmetbme <082aliahmet4@gmail.com>
2023-03-01 18:51:59 +0300

    dslkdsl

--- giris.js
+++ giris.js
@@ -1,5 +1,5 @@
 // console logla merhaba world demek
-console.log("hello world");
+console.log("hello world"); // kullanıcıdan ziyade develeporler görür
 
 // mesaj çıkarmak
 // browserda gösterir bu şekilde
