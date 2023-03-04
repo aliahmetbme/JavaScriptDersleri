5ae0b60373f33f1079f94fabc2384935e6e6cdd8
aliahmetbme <082aliahmet4@gmail.com>
2023-03-04 04:11:55 +0300

    For loop çalışıldı

--- Donguler/ForLoop.js
+++ Donguler/ForLoop.js
@@ -3,6 +3,13 @@
 let users = ["Lorem", "Ipsum", "Dolor", ]
 
 
-for (let index = 0 ; index < 10 ; index++){
-    console.log(index)
-}
+//for (let index = 0 ; index < 10 ; index++){
+ //   console.log(index)
+//}
+
+//let index = 0
+
+//for (; index < 10 ; index++){
+//    console.log(index)
+//}
+
